9ee2bb5c3940b932c82a51af6960dabac04e611c
Nikhil Kothari <nikhilko@google.com>
2015-11-22 20:45:55 -0800

    Add temporary runner code

---
+++ src/tf/main.js
@@ -0,0 +1,29 @@
+// main.js
+// Runs through TensorFlow functionality.
+//
+
+'use strict';
+
+var tensorflow = require('./libtensorflow.js');
+
+var status = tensorflow.createStatus();
+tensorflow.updateStatus(status, 0, 'OK');
+console.log(tensorflow.statusCode(status));
+console.log(tensorflow.statusMessage(status));
+console.log(tensorflow.success(status));
+tensorflow.deleteStatus(status);
+
+var shape = tensorflow.types.LongLongArray([2]);
+var data = tensorflow.types.FloatArray([0.5, 42.0]);
+
+var tensor = tensorflow.createTensor(tensorflow.dataTypes.float, shape, shape.length,
+                                     data.buffer, data.buffer.length);
+console.log('dimensions: ' + tensorflow.tensorDimensions(tensor));
+console.log('dimension[0]: ' + tensorflow.tensorDimensionLength(tensor, 0));
+console.log('data length: ' + tensorflow.tensorDataLength(tensor));
+var buffer = tensorflow.tensorRead(tensor);
+console.log(buffer.readFloatLE(0));
+console.log(buffer.readFloatLE(4));
+
+tensorflow.deleteTensor(tensor);
+
