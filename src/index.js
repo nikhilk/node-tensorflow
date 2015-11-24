4018ae78fa32d56422704bfb3e60ff665a1f3255
Nikhil Kothari <nikhilko@google.com>
2015-11-23 21:48:29 -0800

    Some placeholder files for module API

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// index.js
+// TensorFlow API for node.js applications
+//
+
+'use strict';
+
+var tensorflow = require('./libtensorflow');
+
+module.exports = {
+  Session: require('./session')
+};
+
---
+++ src/session.js
@@ -0,0 +1,11 @@
+// session.js
+// Node.js wrapper for a TensorFlow session and related APIs.
+//
+
+var tensorflow = require('./libtensorflow);
+
+function Session() {
+}
+
+module.exports = Session;
+
